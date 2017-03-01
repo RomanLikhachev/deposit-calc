c0826b0ab89c7b38928e0cdf8978da394f462bc4
RomanLikhachev <romkin63@gmail.com>
2017-03-01 19:20:24 +0400

    Check imputed user data

--- lab2.cpp
+++ lab2.cpp
@@ -13,7 +13,34 @@ int main(){
 	 cin>>srok;
 	 cout<<"Please,type your deposite:";
 	 cin>>sum;
-	 
+	 if (sum<=100000){
+	 	 if (srok<=30) {
+	 		sum=sum-sum*(0.1);
+	 	 
+		  } else if (srok>=31,srok<=120){
+	 		sum=sum+sum*(0.02);
+	 	 
+		  } else if (srok>=121,srok<=240){
+	 		sum=sum+sum*(0.06);
+	 	 
+		  } else if (srok>=241,srok<=365){
+	 		sum=sum+sum*(0.12);
+	 	 }
+	 }
+	 if (sum>100000) {
+	 	 if (srok<=30) {
+	 		sum=sum-sum*(0.1);
+	 	 
+		  } else if (srok>=31,srok<=120){
+	 		sum=sum+sum*(0.03);
+	 	 
+		  } else if (srok>=121,srok<=240){
+	 		sum=sum+sum*(0.08);
+	 	 
+		  } else if (srok>=241,srok<=365){
+	 		sum=sum+sum*(0.15);
+	 	 }
+	 }
 	 cout<<"Your deposit is:"<<sum<<endl;
 	 system("PAUSE");
 }
