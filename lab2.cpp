16554ddf0f01297e4848a7745c95e50d3bd2d3c8
RomanLikhachev <romkin63@gmail.com>
2017-03-01 19:18:08 +0400

    Imput user data

---
+++ lab2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+int main(){
+	int srok,sum,d;
+	 cout<<"Please,type count of days:";
+	 cin>>srok;
+	 cout<<"Please,type your deposite:";
+	 cin>>sum;
+	 
+	 cout<<"Your deposit is:"<<sum<<endl;
+	 system("PAUSE");
+}
